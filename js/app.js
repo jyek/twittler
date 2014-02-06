3c7c11348e25c6d8448477e345e0425ce23463b4
jyek <justinyek@gmail.com>
2014-02-06 13:24:34 +0800

    Basic requirements met

--- js/app.js
+++ js/app.js
@@ -2,40 +2,59 @@ $(document).ready(function(){
 	/* Parameters */
   var $tweetContainer = $('.tweet-content');
   var index = -1;
-	var refreshRate = 1000;
+	var refreshRate = 3000;
 	var user = null;
+
+	/* Show all tweets button */
+	var $showAllButton = $('<span class="onclick-show-all"><i class="fa fa-home"></i> All Tweets</span>');
+	$showAllButton.click(function(){
+		user = null;
+		$('.tweet-subheader').empty();
+		$('.onclick-show-all').detach();
+		refreshTweets();
+	})
 
 	/* Refresh tweets */
 	var refreshTweets = function(){
-		var index = streams.home.length - 1;
+
+		// get tweets
+		var tweets;
+		if (user === null){
+			tweets = streams.home;
+		} else {
+			tweets = streams.users[user];
+		}
+		
+		// refresh view
+		var index = tweets.length - 1;
 		$tweetContainer.empty();
     while(index >= 0){
-      var tweet = streams.home[index];
-
-			if (user === null || user === tweet.user){
-	      var $tweet = $('<div class="tweet"></div>');
-				var $tweetUser = $('<div class="username"></div>');
-				var $tweetTime = $('<div class="timestamp"></div>');
-				var $tweetMsg = $('<div class="msg"></div>');
-				$tweetUser.text('@' + tweet.user);
-				$tweetTime.text( moment(tweet.created_at).fromNow() );
-				$tweetMsg.text(tweet.message);
-				$tweetUser.appendTo($tweet);
-				$tweetTime.appendTo($tweet);
-				$tweetMsg.appendTo($tweet);
-	      $tweet.appendTo($tweetContainer);
-			}
-			
+      var tweet = tweets[index];
+      var $tweet = $('<div class="tweet"></div>');
+			var $tweetUser = $('<div class="username"></div>');
+			var $tweetTime = $('<div class="timestamp"></div>');
+			var $tweetMsg = $('<div class="msg"></div>');
+			$tweetUser.text('@' + tweet.user);
+			$tweetTime.text( moment(tweet.created_at).fromNow() );
+			$tweetMsg.text(tweet.message);
+			$tweetUser.appendTo($tweet);
+			$tweetTime.appendTo($tweet);
+			$tweetMsg.appendTo($tweet);
+      $tweet.appendTo($tweetContainer);
       index -= 1;
     }
 		
-		/* username click filters tweets by user */
+		// when clicked, filters tweets by user
 		$('.username').click(function(){
 			user = $(this).text().slice(1);
-			$('.tweet-subheader').text('Tweets by @' + user);
+			$('.tweet-subheader').append('Tweets by @' + user);
+			if ( $('.onclick-show-all').length === 0){
+				$('.tweet-nav').prepend($showAllButton);
+			}
+			refreshTweets();
 		});
 	}
-
+	
 	/* Auto refreshes tweets after every time interval */
 	var autorefreshTweets = function(){
 		refreshTweets();
@@ -43,6 +62,7 @@ $(document).ready(function(){
 			autorefreshTweets();
 		},refreshRate);
 	}
-		
+
+	/* Kickstart program */
 	autorefreshTweets();
 });
